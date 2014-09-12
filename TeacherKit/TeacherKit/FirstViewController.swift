da45bb8de142f128770e874a4e05785ca11d50be
Ramy Medhat <rmedhat@uwaterloo.ca>
2014-09-12 17:58:21 -0400

    First Commit

---
+++ TeacherKit/TeacherKit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TeacherKit
+//
+//  Created by Ramy Medhat on 2014-09-12.
+//  Copyright (c) 2014 BitKit. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
